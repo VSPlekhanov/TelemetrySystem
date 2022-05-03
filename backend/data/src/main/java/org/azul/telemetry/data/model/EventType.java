d7cd34dd42570197d521da15e9e4e1c71c8caf12
vsplekhanov <sclerothorax@gmail.com>
2022-05-04 00:01:48 +0300

    Almost finished with PoC

--- backend/data/src/main/java/org/azul/telemetry/data/model/EventType.java
+++ backend/data/src/main/java/org/azul/telemetry/data/model/EventType.java
@@ -2,5 +2,21 @@ package org.azul.telemetry.data.model;
 
 
 public enum EventType {
-    STARTUP, SHUTDOWN, UPDATE
+    STARTUP(Names.STARTUP), SHUTDOWN(Names.SHUTDOWN), UPDATE(Names.UPDATE);
+
+    public static class Names{
+        public static final String STARTUP = "STARTUP";
+        public static final String SHUTDOWN = "SHUTDOWN";
+        public static final String UPDATE = "UPDATE";
+    }
+
+    private final String label;
+
+    EventType(String label) {
+        this.label = label;
+    }
+
+    public String toString() {
+        return this.label;
+    }
 }
