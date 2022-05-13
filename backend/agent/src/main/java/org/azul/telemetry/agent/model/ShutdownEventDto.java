e43b9b73625b0a3e4bb0fdac07be48aa995b4b34
vsplekhanov <sclerothorax@gmail.com>
2022-05-13 16:34:29 +0300

    fix shutdown and update evetns dtos

--- backend/agent/src/main/java/org/azul/telemetry/agent/model/ShutdownEventDto.java
+++ backend/agent/src/main/java/org/azul/telemetry/agent/model/ShutdownEventDto.java
@@ -5,11 +5,13 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.EqualsAndHashCode;
 import lombok.Value;
 import lombok.experimental.SuperBuilder;
+import lombok.extern.jackson.Jacksonized;
 
 import javax.validation.constraints.NotNull;
 
 @EqualsAndHashCode(callSuper = true)
 @Value
+@Jacksonized
 @SuperBuilder
 public class ShutdownEventDto extends EventDto {
     @NotNull
